0400a1447ed8a2d913258c95dbd96919813d034f
sulha199 <sulha199@gmail.com>
2023-06-27 12:15:18 +0700

    fix: display tsc-errors only from staged files

--- lib/file-type-check.ts
+++ lib/file-type-check.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import fs from "node:fs";
 import process from "node:process";
+import { relative, isAbsolute,  } from "path"
 import { createTsConfig } from "./create-tsconfig";
 
 import { runTypescript } from "./run-typescript";
@@ -19,8 +20,20 @@ const fileTypeCheck = async (stagedFiles: string[]) => {
   } catch (error) {
     fs.unlinkSync(radomizedFileName);
     if (error instanceof ErrorExec) {
-      console.log(error.stdout);
-      process.exitCode = error.error?.code || 1;
+      const currentPath = process.cwd();
+      const errorMessage = (error as ErrorExec).stdout;
+      const newLine = "\r\n";
+      const linesFromError = errorMessage.split(newLine)
+      const getRelativePath = (absolutePath: string) => (isAbsolute(absolutePath) ? relative(currentPath, absolutePath) : absolutePath).replaceAll("\\","/");
+      const errorsFromStagedFiles: string[] = [];
+      stagedFiles.forEach(absolutePath => {
+        const relativePath = getRelativePath(absolutePath);
+        errorsFromStagedFiles.push(...linesFromError.filter(line => line.includes(relativePath)));
+      })
+      if (errorsFromStagedFiles.length > 0) {
+        console.log(errorsFromStagedFiles.join(newLine))
+        process.exitCode = error.error?.code || 1;
+      }
     } else {
       console.log(error);
       process.exitCode = 1;
